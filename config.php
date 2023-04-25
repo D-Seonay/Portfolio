e76811b7bc35bed20606c23f40921a5826b04d6e
Seonay <91608025+D-Seonay@users.noreply.github.com>
2023-04-25 11:47:30 +0200

    Delete config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class config
-{
-    const SERVEUR = "localhost";
-    const BDD = "u791543610_portfolio";
-    const UTILISATEUR = "u791543610_seonay";
-    const MOTDEPASSE = "64XKFwF,e$[y)t[";
-}
